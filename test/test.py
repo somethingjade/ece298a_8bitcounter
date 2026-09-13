6750d3693dc8ff8c56557fc8f82b0486b6768fd9
Ronson <artizanw1@gmail.com>
2026-09-13 06:43:32 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -32,7 +32,7 @@ async def test_project(dut):
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
-    dut._log.info(f"actual value: {dut.uo_out.integer}")
+    dut._log.info(f"actual value: {dut.uo_out.value.integer}")
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
