e597c017b550a0bda6b94c15e79493c8431e47e3
Ronson <artizanw1@gmail.com>
2026-09-13 06:51:54 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -27,7 +27,7 @@ async def test_project(dut):
 
     # Set the input values you want to test
     dut.ui_in.value = 20
-    dut.uio_in.value = 0b00000011
+    dut.uio_in.value = 0b11111111
     
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
