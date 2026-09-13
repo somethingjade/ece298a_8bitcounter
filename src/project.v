6e2abf655dd9b9c2be9a868655a8e6a20b2dfe5d
Ronson <artizanw1@gmail.com>
2026-09-13 05:39:42 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,8 +16,23 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+    reg [7:0] count;
+    // uio_in[0] is load
+    // uio_in[1] is output_en
+    
+    always @(posedge clk) begin
+        if (!rst_n)
+            count <= 0;
+        else if (uio_in[0])
+            count <= ui_in;
+        else
+            count <= count + 1;
+    end
+
+    assign uo_out = uio_in[1] ? count : 8'bz;
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
