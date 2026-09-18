bca80cf239ba1f6b492cdc443a031c05024b3de7
artizanw <artizanw1@gmail.com>
2026-09-17 20:17:42 -0400

    woops

--- src/project.v
+++ src/project.v
@@ -21,15 +21,18 @@ module tt_um_example (
 	// uio_in[1] is output_en
 
 	always @(posedge clk or negedge rst_n) begin
-		if (!rst_n)
-			count <= 0;
-		else if (uio_in[0])
-			count <= ui_in;
+		if (uio_in[1])
+			if (!rst_n)
+				count <= 0;
+			else if (uio_in[0])
+				count <= ui_in;
+			else
+				count <= count + 1;
 		else
-			count <= count + 1;
+			count <= 8'bz;
 	end
 
-	assign uo_out = uio_in[1] ? count : 8'bz;
+	assign uo_out = count;
 
 	// All output pins must be assigned. If not used, assign to 0.
 	// assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
