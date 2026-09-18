404b1a461483231cad2ef717951a4ef57681623c
artizanw <artizanw1@gmail.com>
2026-09-17 22:36:03 -0400

    fix load test function. add ui_in to unused

--- src/project.v
+++ src/project.v
@@ -47,6 +47,6 @@ module tt_um_example (
 	assign uo_out = 0;
 
 	// List all unused inputs to prevent warnings
-	wire _unused = &{ena, ui_in[7:4], 1'b0};
+	wire _unused = &{ena, uio_in, ui_in[7:4], 1'b0};
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -9,9 +9,9 @@ async def load(dut, value, *, output_en = 1):
     for i in range(0, 8):
         dut.ui_in.value = 0b00000010 | (output_en << 3) | (((1 << i) & value) >> i)
         await ClockCycles(dut.clk, 1)
-        dut.ui_in.value = 0b00000100 | (output_en << 3)
-        await ClockCycles(dut.clk, 1)
-        dut.ui_in.value = 0b00000000 | (output_en << 3)
+    dut.ui_in.value = 0b00000100 | (output_en << 3)
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 0b00000000 | (output_en << 3)
 
 
 @cocotb.test()
